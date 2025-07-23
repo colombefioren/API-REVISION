2819de242e64e3d960918ddb2ed3859ceca0a069
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-23 15:43:28 +0300

    feat: accept type check

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import json
-
 from fastapi import FastAPI
+from starlette.requests import Request
 from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
 @app.get("/hello")
-def hello(name:str = "Non défini(e)",is_teacher:bool = False):
-    if is_teacher is True:
-        return JSONResponse({"message" : f"Hello there teacher {name}!"})
-    if is_teacher is False:
-        if name != "Non défini(e)":
-            return JSONResponse({"message": f"Hello there {name}!"})
-    return Response(content=json.dumps({"message":"Hello there bitch!"}),status_code=200,media_type="application/json")
+def hello(request : Request,name:str = "Non défini(e)",is_teacher:bool = False):
+    accept_type = request.headers.get("accept")
+    if accept_type not in ("text/plain","text/html"):
+        return "You won't see shit bitch!"
+    else:
+        if is_teacher is True:
+            return JSONResponse({"message" : f"Hello there teacher {name}!"})
+        else:
+            if name != "Non défini(e)":
+                return JSONResponse({"message": f"Hello there {name}!"})
+            return Response(content=json.dumps({"message":"Hello there bitch!"}),status_code=200,media_type="application/json")
