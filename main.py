22d01957b9c4f95a31f6f69fdfb06dc8cc868bbb
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-23 15:23:09 +0300

    refactor: for optional parameters with different outputs

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ app = FastAPI()
 @app.get("/hello")
 def hello(name:str = "Non défini(e)",is_teacher:bool = False):
     if is_teacher is True:
-        return JSONResponse({f"Hello there teacher {name}!"})
+        return JSONResponse({"message" : f"Hello there teacher {name}!"})
+    if is_teacher is False:
+        if name != "Non défini(e)":
+            return JSONResponse({"message": f"Hello there {name}!"})
     return Response(content=json.dumps({"message":"Hello there bitch!"}),status_code=200,media_type="application/json")
