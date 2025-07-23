e003f240f43a578a6810e5bdb26c15e1b7ddc850
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-23 15:01:34 +0300

    feat: accept name and is_teacher params in hello endpoint

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
 
 from fastapi import FastAPI
-from starlette.responses import Response
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
 @app.get("/hello")
-def hello():
+def hello(name:str = "Non défini(e)",is_teacher:bool = False):
+    if is_teacher is True:
+        return JSONResponse({f"Hello there teacher {name}!"})
     return Response(content=json.dumps({"message":"Hello there bitch!"}),status_code=200,media_type="application/json")
